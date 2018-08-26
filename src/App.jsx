824ae7c550748e5e9028fd28bbfde327a4fcadb8
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 01:16:11 -0500

    synth noise

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
 import React, { Component } from 'react';
-import Clock from './Clock';
-import './App.css';
-import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deadline: 'December 25, 2017',
-      newDeadline: ''
-    }
-  }
-
-  changeDeadline() {
-    this.setState({deadline: this.state.newDeadline});
-  }
-
-  render() {
+  render(){
     return (
-      <div className="App">
-        <div className="App-title">
-          Countdown to {this.state.deadline}
-        </div>
-        <Clock
-          deadline={this.state.deadline}
-        />
-        <Form inline>
-          <FormControl
-            className="Deadline-input"
-            placeholder='new date'
-            onChange={event => this.setState({newDeadline: event.target.value})}
-          />
-          <Button onClick={() => this.changeDeadline()}>
-            Submit
-          </Button>
-        </Form>
-      </div>
+    <div>The Final Countdown App Component</div>
     )
   }
 }
