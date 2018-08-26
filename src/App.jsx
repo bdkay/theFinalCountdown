58e25e1866f9157c6ee5817aca8d69e9b7d09a99
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 02:02:12 -0500

    The State

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      deadline: 'December 31st, 2099'
+    }
+  }
+  
   render(){
     return (
     <div className="App">
-      <div className="App-title">The Final Countdown</div>
+      <div className="App-title">
+        The Final Countdown
+      </div>
+      <div className="App-subtitle">
+        {this.state.deadline}
+      </div>
       <div>
         <div className="clock-days">14 Days, </div>
         <div className="clock-hours">30 Hours, </div>
         <div className="clock-minutes">15 Minutes, </div>
         <div className="clock-seconds">20 Seconds</div>
       </div>
-      <div className="App-subtitle">
+      <div className="App-submit">
         <input placeholder="New Date" />
         <button>Submit</button>
       </div>
