1bf0123a3f4012284946ba59ebd36f23d50d3f0e
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 03:20:00 -0500

    The final countdown 1

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -16,15 +16,19 @@ class Clock extends Component  {
     this.getTimeUntil(this.props.deadline);
   }
   
+  componentDidMount(){
+    setInterval(() => {
+      this.getTimeUntil((this.props.deadline), 1000);
+    })
+  }
+  
   getTimeUntil(deadline){
     const time = Date.parse(deadline) - Date.parse(new Date());
-    console.log('time', time);
     const seconds = Math.floor((time/1000) % 60);
     const minutes = Math.floor((time/1000/60) % 60);
     const hours = Math.floor(time/(1000*60*60) % 24);
     const days = Math.floor(time/(1000*60*60*24));
     
-    console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
     this.setState({
       days,
       hours,
