a948d9234b387721e5a71f3d366e4aa98c6fac8d
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 01:48:10 -0500

    Europe

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render(){
     return (
-    <div>The Final Countdown App Component</div>
+    <div className="App">
+      <div className="App-title">The Final Countdown</div>
+      <div>
+        <div className="clock-days">14 Days, </div>
+        <div className="clock-hours">30 Hours, </div>
+        <div className="clock-minutes">15 Minutes, </div>
+        <div className="clock-seconds">20 Seconds</div>
+      </div>
+      <div className="App-subtitle">
+        <input placeholder="New Date" />
+        <button>Submit</button>
+      </div>
+    </div>
     )
   }
 }
