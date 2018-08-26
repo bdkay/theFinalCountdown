2de5a1e09631c8baf89b1e5e8fba82978189c01f
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 02:33:24 -0500

    return

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -4,15 +4,23 @@ import './App.css';
 class Clock extends Component  {
   constructor(props){
     super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
   }
   
-  render(){
-    <div>
-      <div className="clock-days">14 Days, </div>
-      <div className="clock-hours">30 Hours, </div>
-      <div className="clock-minutes">15 Minutes, </div>
-      <div className="clock-seconds">20 Seconds</div>
-    </div>
+  render() {
+    return (
+      <div>
+        <div className="clock-days">14 Days, </div>
+        <div className="clock-hours">30 Hours, </div>
+        <div className="clock-minutes">15 Minutes, </div>
+        <div className="clock-seconds">20 Seconds</div>
+      </div>
+    )
   }
 }
 
