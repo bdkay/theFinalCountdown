d8c4df4a35b2c62a955a100f19532fb9276ba4c4
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-26 03:16:09 -0500

    lifecycle events

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -12,6 +12,10 @@ class Clock extends Component  {
     };
   }
   
+  componentWillMount(){
+    this.getTimeUntil(this.props.deadline);
+  }
+  
   getTimeUntil(deadline){
     const time = Date.parse(deadline) - Date.parse(new Date());
     console.log('time', time);
@@ -21,10 +25,12 @@ class Clock extends Component  {
     const days = Math.floor(time/(1000*60*60*24));
     
     console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+    this.setState({
+      days: days
+    });
   }
   
   render() {
-    this.getTimeUntil(this.props.deadline);
     return (
       <div>
         <div className="clock-days">{this.state.days} Days</div>
